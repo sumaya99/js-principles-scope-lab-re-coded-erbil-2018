3596013eff24e96a4956a43bd52c8aae06846e51
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-13 04:21:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteCustomer = 'sour fred';
+
+function upperCaseCustomerName () {
+  return customerName = customerName.toUpperCase();
+}
+
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = 'sour fred';
+}
+
+function setBestCustomer () {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer (newFavorite) {
+  bestCustomer = newFavorite;
+}
+
+
